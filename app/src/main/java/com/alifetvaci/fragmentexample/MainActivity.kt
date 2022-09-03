23da01239351020063c1d69a5793371691b68dc6
aliFetvaci61 <alifetvaci61@hotmail.com>
2022-09-03 17:07:07 +0300

    fragment example

---
+++ app/src/main/java/com/alifetvaci/fragmentexample/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.alifetvaci.fragmentexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.alifetvaci.fragmentexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fragment1Button.setOnClickListener {
+            replaceFragment(Fragment1())
+        }
+        binding.fragment2Button.setOnClickListener {
+            replaceFragment(Fragment2())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainerView,fragment)
+        fragmentTransaction.commit()
+    }
+}
